33e5b4f7d2ae898fbc34c0508462c4a4ce562dac
Michael Mount <michaeljoesphmount@gmail.com>
2026-05-06 19:27:30 -0400

    built simple navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
@@ -9,15 +11,17 @@ export default function Home() {
         <section className="container mx-auto px-4 py-32">
           <div className="mx-auto max-w-4xl text-center">
             <h1 className="text-black mb-6 text-6xl font-bold ">
-              A better way to track your job applictions
+              A better way to track your job applications
             </h1>
             <p className="text-muted-foreground mb-10 text-xl">
-              Capture, organize, and mange your job search in one place
+              Capture, organize, and manage your job search in one place
             </p>
             <div className="flex flex-col items-center gap-4">
-              <Button size="lg" className="h-12 px-8 text-lg font-medium">
-                Start for Free <ArrowRight className="ml-2" />
-              </Button>
+              <Link href="/sign-up">
+                <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                  Start for Free <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
               <p className="text-sm text-muted-foreground">
                 Free forever. No Credit Card Required
               </p>
